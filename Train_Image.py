467e6cd07a11460d4f52af269a383e981fb6b0e1
dhhruv <180320107529.ce.dhruv@gmail.com>
2021-02-27 22:32:24 +0530

    Initial Commit

---
+++ Train_Image.py
@@ -0,0 +1,55 @@
+import os
+import time
+import cv2
+import numpy as np
+from PIL import Image
+from threading import Thread
+
+
+
+# -------------- image labesl ------------------------
+
+def getImagesAndLabels(path):
+    # get the path of all the files in the folder
+    imagePaths = [os.path.join(path, f) for f in os.listdir(path)]
+    # print(imagePaths)
+
+    # create empth face list
+    faces = []
+    # create empty ID list
+    Ids = []
+    # now looping through all the image paths and loading the Ids and the images
+    for imagePath in imagePaths:
+        # loading the image and converting it to gray scale
+        pilImage = Image.open(imagePath).convert('L')
+        # Now we are converting the PIL image into numpy array
+        imageNp = np.array(pilImage, 'uint8')
+        # getting the Id from the image
+        Id = int(os.path.split(imagePath)[-1].split(".")[1])
+        # extract the face from the training image sample
+        faces.append(imageNp)
+        Ids.append(Id)
+    return faces, Ids
+
+
+# ----------- train images function ---------------
+def TrainImages():
+    recognizer = cv2.face_LBPHFaceRecognizer.create()
+    harcascadePath = "haarcascade_frontalface_default.xml"
+    detector = cv2.CascadeClassifier(harcascadePath)
+    faces, Id = getImagesAndLabels("TrainingImage")
+    Thread(target = recognizer.train(faces, np.array(Id))).start()
+    # Below line is optional for a visual counter effect
+    Thread(target = counter_img("TrainingImage")).start()
+    recognizer.save("TrainingImageLabel"+os.sep+"Trainner.yml")
+    print("All Images")
+
+# Optional, adds a counter for images trained (You can remove it)
+def counter_img(path):
+    imgcounter = 1
+    imagePaths = [os.path.join(path, f) for f in os.listdir(path)]
+    for imagePath in imagePaths:
+        print(str(imgcounter) + " Images Trained", end="\r")
+        time.sleep(0.008)
+        imgcounter += 1
+
---
+++ check_camera.py
@@ -0,0 +1,35 @@
+
+def camer():
+    import cv2
+
+    # Load the cascade
+    face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+    # To capture video from webcam.
+    cap = cv2.VideoCapture(0)
+
+    while True:
+        # Read the frame
+        _, img = cap.read()
+
+        # Convert to grayscale
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+        # Detect the faces
+        faces = face_cascade.detectMultiScale(gray, 1.3, 5, minSize=(30, 30),flags = cv2.CASCADE_SCALE_IMAGE)
+
+        # Draw the rectangle around each face
+        for (x, y, w, h) in faces:
+            cv2.rectangle(img, (x, y), (x + w, y + h), (10,159,255), 2)
+
+
+        # Display
+        cv2.imshow('Webcam Check', img)
+
+        # Stop if escape key is pressed
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    # Release the VideoCapture object
+    cap.release()
+    cv2.destroyAllWindows()
